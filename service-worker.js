6c8c1212f95132b2f17f8af06df5cbaefabb1cf1
fonteantonio1977-hub <fonte.antonio1977@gmail.com>
2025-10-16 11:44:33 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,7 @@
-
-const CACHE = 'inr-coumadin-ghp-full-hub-v1';
-const PRECACHE_URLS = [
-  'https://fonteantonio1977-hub.github.io/terapia-inr/',
-  'https://fonteantonio1977-hub.github.io/terapia-inr/index.html',
-  'https://fonteantonio1977-hub.github.io/terapia-inr/404.html',
-  'https://fonteantonio1977-hub.github.io/terapia-inr/manifest.json',
-  'https://fonteantonio1977-hub.github.io/terapia-inr/icon-192.png',
-  'https://fonteantonio1977-hub.github.io/terapia-inr/icon-512.png',
-  'https://fonteantonio1977-hub.github.io/terapia-inr/apple-touch-icon.png',
-  'https://unpkg.com/react@18/umd/react.production.min.js','https://unpkg.com/react-dom@18/umd/react-dom.production.min.js','https://unpkg.com/recharts@2.12.7/umd/Recharts.min.js','https://unpkg.com/@babel/standalone@7.23.9/babel.min.js'
+const CACHE='inr-coumadin-nosjsx-v2';const PRECACHE=[
+'./','./index.html','./404.html','./manifest.json','./app.js','./icon-192.png','./icon-512.png','./apple-touch-icon.png',
+'https://unpkg.com/react@18/umd/react.production.min.js','https://unpkg.com/react-dom@18/umd/react-dom.production.min.js','https://unpkg.com/recharts@2.12.7/umd/Recharts.min.js'
 ];
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(PRECACHE_URLS)).then(() => self.skipWaiting()));
-});
-self.addEventListener('activate', (event) => { event.waitUntil(self.clients.claim()); });
-self.addEventListener('fetch', (event) => {
-  const url = new URL(event.request.url);
-  if (PRECACHE_URLS.includes(url.href)) {
-    event.respondWith(
-      caches.open(CACHE).then(cache => cache.match(event.request).then(resp => resp || fetch(event.request).then(r => { cache.put(event.request, r.clone()); return r; })))
-    ); return;
-  }
-  event.respondWith(fetch(event.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(event.request,c));return r;}).catch(()=>caches.match(event.request).then(m=>m||caches.match('https://fonteantonio1977-hub.github.io/terapia-inr/index.html'))));
-});
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(PRECACHE)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
